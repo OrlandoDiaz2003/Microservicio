e47869529388b168c08e10b7fba06b1869b0a451
Orlando <or.diaz@duocuc.cl>
2025-05-07 14:35:08 -0400

    relacion entre tabla

--- catalogo/src/main/java/com/catalogo/catalogo/Model/Categoria.java
+++ catalogo/src/main/java/com/catalogo/catalogo/Model/Categoria.java
@@ -5,35 +5,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.util.*;
 @Entity
-@Table(name = "Producto")
+@Table(name = "Categoria")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Producto {
+public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int productoId;
+    private int categoriaId;
 
     @Column(nullable = false)
-    private String nombre;
+    private String descripcion;
 
-    @Column(nullable = false)
-    private int stock;
-
-    @Column(nullable = false)
-    private double precio;
-
-    @ManyToOne
-    @JoinColumn(name = "categoria_id")
-    private Categoria categoria;
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
 
 }
