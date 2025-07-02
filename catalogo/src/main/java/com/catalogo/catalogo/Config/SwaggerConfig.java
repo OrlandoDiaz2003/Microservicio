31835f8083818bf19f33e02133940c8d3e7ba0bf
Orlando <or.diaz@duocuc.cl>
2025-07-02 19:42:28 -0400

    swagger fix

--- catalogo/src/main/java/com/catalogo/catalogo/Config/SwaggerConfig.java
+++ catalogo/src/main/java/com/catalogo/catalogo/Config/SwaggerConfig.java
@@ -1,10 +1,12 @@
 package com.catalogo.catalogo.Config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+// swagger url http://localhost:8080/doc/swagger-ui/index.html#/
 @Configuration
 public class SwaggerConfig {
 
